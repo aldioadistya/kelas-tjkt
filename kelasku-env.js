e629225365171e5df6b04e3485def4624e6c81b5
aldioadistya <aldioadistya@gmail.com>
2026-09-18 19:44:19 +0700

    Create kelasku-env.js

---
+++ kelasku-env.js
@@ -0,0 +1,16 @@
+// Kelasku runtime environment.
+// Firebase browser config adalah konfigurasi client publik;
+// jangan menaruh server secret di file ini.
+
+window.KELASKU_ENV = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAhrh00F2N07_Lx-QT6sHi_ygsvWfds1eQ",
+    authDomain: "apps-kelas.firebaseapp.com",
+    projectId: "apps-kelas",
+    storageBucket: "apps-kelas.firebasestorage.app",
+    messagingSenderId: "24953961039",
+    appId: "1:24953961039:web:4ad80d05e9f4be771c083a"
+  },
+
+  driveApiUrl: "https://script.google.com/macros/s/AKfycbxkCOpaqCYnQ7HOgK61t189lvHap3wCjP9jj9ks73XbQJdWgHFnGUdye0hxjoh8Z1mveQ/exec"
+};
